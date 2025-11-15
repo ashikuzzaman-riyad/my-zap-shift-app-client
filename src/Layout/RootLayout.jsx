468c9350f036fe101585f6182d3af2376ff85471
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-15 20:40:58 +0600

    added navbaer

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../Page/Shared/Navbar/Navbar';
+import Footer from '../Page/Shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
-        <div>
-            <Navbar></Navbar>
-           <Outlet></Outlet> 
+        <div className='flex flex-col min-h-screen'>
+            <header>
+                <Navbar></Navbar>
+            </header>
+           <main className='flex-1'>
+            <Outlet></Outlet> 
+           </main>
+          <section>
+             <Footer></Footer>
+          </section>
         </div>
     );
 };
--- src/Page/Shared/Footer/Footer.jsx
+++ src/Page/Shared/Footer/Footer.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import { FaLinkedin, FaFacebook, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import Logo from "../../../Logo/Logo";
 
 const Footer = () => {
     return (
+        <footer className=" text-gray-300 py-10 mt-20">
+      <div className="max-w-7xl mx-auto px-5 gap-10 text-center space-y-4">
+
+        {/* Left Section */}
         <div>
-            
+            <div className="flex  items-center justify-center">
+            <Logo></Logo>
+          <h2 className="text-3xl font-bold text-white">ZapShift</h2>
+            </div>
+          <p className="mt-3 text-sm ">
+            Enjoy fast, reliable parcel delivery with real-time tracking and zero hassle. 
+            From personal packages to <br />business shipments — we deliver on time, every time.
+          </p>
         </div>
+        <ul className="md:flex items-center justify-center gap-6 font-semibold hidden">
+            <li>Services</li>
+            <li>Coverage</li>
+            <li>About Us</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* Social Icons */}
+        <div className=" flex flex-col items-center">
+          
+          <div className="flex items-center gap-5 text-xl">
+
+            <a 
+              href="#" 
+              className="hover:text-emerald-400 transition"
+            >
+              <FaLinkedin />
+            </a>
+
+            <a 
+              href="#" 
+              className="hover:text-emerald-400 transition"
+            >
+              <FaXTwitter />
+            </a>
+
+            <a 
+              href="#" 
+              className="hover:text-emerald-400 transition"
+            >
+              <FaFacebook />
+            </a>
+
+            <a 
+              href="#" 
+              className="hover:text-emerald-400 transition"
+            >
+              <FaYoutube />
+            </a>
+
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom */}
+      <div className="text-center text-sm text-gray-500 mt-10 border-t border-gray-700 pt-5">
+        © {new Date().getFullYear()} ZapShift — All rights reserved.
+      </div>
+    </footer>
     );
 };
 
